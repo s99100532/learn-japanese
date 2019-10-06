2e867d3cfa3e7a013fd3b9f0457eda19f887779f
david9328 <s99100532@gmail.com>
2019-10-06 16:38:10 +0800

    improve play audio

--- src/components/CharacterAudio.tsx
+++ src/components/CharacterAudio.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 type Props = {
     char: string
@@ -7,16 +7,25 @@ type Props = {
 
 const CharacterAudio = ({ char }: Props) => {
 
-    const [audio, setAudio] = useState("");
+    const [audio, setAudio] = useState<HTMLAudioElement>();
     useEffect(() => {
         import(`../audio/${char.replace(/_/g, "")}.mp3`).then(audio => {
-            setAudio(audio.default);
+            setAudio(new Audio(audio.default));
         });
     }, [char]);
 
+    const playAudio = useCallback(e => {
+        e.preventDefault();
+        if(audio) {
+            audio.play();
+        } else {
+            alert('audio is not avaliable');
+        }
+    }, [audio]);
+
 
     return (
-        <button className="button is-rounded" onClick={e => { e.preventDefault(); (new Audio(audio)).play() }}>
+        <button className="button is-rounded" onClick={playAudio}>
             <span className="icon is-small">
                 <i className="fas fa-play"></i>
             </span>
